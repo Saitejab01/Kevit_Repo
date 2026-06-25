1d08f4ecd90837cf5283c77efbc827e0cf0824b6
Saitejab01 <saitejab01@gmail.com>
2026-06-25 10:12:34 +0530

    commit message

--- src/main/java/logic/Test.java
+++ src/main/java/logic/Test.java
@@ -3,8 +3,8 @@ package logic;
 public class Test {
 
 	public static void main(String[] args) {
-		System.out.println("Hello");
 		System.out.println("Hi") ;
+		System.out.println("Hello");
 	}
 
 }
